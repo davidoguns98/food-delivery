c936f034720d431fb3ecdad0526ed353c9fd3c4a
davidoguns98 <142021415+davidoguns98@users.noreply.github.com>
2024-06-22 06:27:27 +0100

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
-import { CiSearch } from "react-icons/ci";
+
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { assets } from "../../assets/assests/assests";
 import { StoreContext } from "../../context/StoreContext";
